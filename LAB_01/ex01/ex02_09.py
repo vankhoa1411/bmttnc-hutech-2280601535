7ebf3591c39b91de6a30eafb58bafef2916df819
vankhoa1411 <tenkhoatui13@gmail.com>
2025-05-29 09:27:15 +0700

    Add files via upload

---
+++ LAB_01/ex01/ex02_09.py
@@ -0,0 +1,16 @@
+def kiem_tra_so_nguyen_to(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+try:
+    number = int(input("Nhập vào số cần kiểm tra: "))
+    if kiem_tra_so_nguyen_to(number):
+        print(number, "là số nguyên tố.")
+    else:
+        print(number, "không phải là số nguyên tố.")
+except ValueError:
+    print("Vui lòng nhập một số nguyên hợp lệ.")
